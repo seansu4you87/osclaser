37024b9455afc5f6cde4c62569572112049ed27e
codeb87 <codeb87@ba1854fa-1114-11df-8588-0bb593d8ec56>
2010-02-03 23:35:51 +0000

    skeletons of shared classes

--- trunk/SharedCollection.cpp
+++ trunk/SharedCollection.cpp
@@ -1,2 +1,32 @@
 #include "stdafx.h"
 #include "SharedCollection.h"
+#include "SharedObject.h"
+
+#include "OscReceivedElements.h"
+#include "OscPacketListener.h"
+#include "UdpSocket.h"
+
+SharedCollection::SharedCollection()
+{
+
+}
+
+void SharedCollection::addSharedObject(SharedObject * newObject)
+{
+
+}
+
+void SharedCollection::removeSharedObject(SharedObject * deletedObject)
+{
+
+}
+
+void SharedCollection::processNewMessage(const osc::ReceivedMessage & m)
+{
+
+}
+
+SharedObject * SharedCollection::objectWithID(int objectID)
+{
+	return NULL;
+}
--- trunk/SharedObject.h
+++ trunk/SharedObject.h
@@ -1,1 +1,15 @@
 //SharedObject.h
+#pragma once
+#include <string>
+
+using namespace std;
+
+class osc::ReceivedMessage;
+
+class SharedObject {
+	public:
+		int objectID;
+		string objectName;
+		void setFromMessage(const osc::ReceivedMessage&);
+
+};
