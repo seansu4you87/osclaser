46aa2230b964cdfacf87d41db0ec1551b2ceda0c
codeb87 <codeb87@ba1854fa-1114-11df-8588-0bb593d8ec56>
2010-02-05 11:42:08 +0000

    put in shit for points

--- trunk/SharedObject.h
+++ trunk/SharedObject.h
@@ -11,5 +11,9 @@ class SharedObject {
 		int objectID;
 		string objectName;
 		virtual void setFromMessage(const osc::ReceivedMessage&);
+		SharedObject();
+	private:
+		LONG * points;
+		int numPoints;
 
 };
